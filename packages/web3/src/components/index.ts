e1ff7e967d34b7d4c30ec74ce5bcbe4db24a2f09
wangyu <13100684032@163.com>
2024-01-03 17:26:49 +0800

    add

--- packages/web3/src/components/index.ts
+++ packages/web3/src/components/index.ts
@@ -1,3 +1,5 @@
 import ErrorBoundary from './errorBoundary'
 
-export {ErrorBoundary}
+export {
+    ErrorBoundary
+}
--- packages/web3/src/main.tsx
+++ packages/web3/src/main.tsx
@@ -1,5 +1,4 @@
 import React from "react"
 import ReactDOM from 'react-dom/client'
 import App from "./app";
-
 ReactDOM.createRoot(document.getElementById("app") as HTMLElement).render(<App/>)
